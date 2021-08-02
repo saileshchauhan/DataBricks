1b3271cdc23791a5135a587c98217b28efbe7f79
saileshchauhan <chauhansailesh68@gmail.com>
2021-08-02 13:10:03 +0530

    [sailesh] add: sentiment analysic spark streaming local machine

---
+++ localMachine_pyspark_streaming/twitter_socket.py
@@ -0,0 +1,69 @@
+
+import tweepy
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+import socket
+import json
+from decouple import config
+
+
+
+consumer_key=config('consumer_key')
+consumer_secret=config('consumer_secret')
+access_token =config('access_token')
+access_secret=config('access_secret')
+
+class TweetsListener(StreamListener):
+  # tweet object listens for the tweets
+  def __init__(self, csocket):
+    self.client_socket = csocket
+  def on_data(self, data):
+    try:  
+      msg = json.loads( data )
+      print("new message")
+      # if tweet is longer than 140 characters
+      if "extended_tweet" in msg:
+        # add at the end of each tweet "t_end" 
+        self.client_socket\
+            .send(str(msg['extended_tweet']['full_text']+"t_end")\
+            .encode('utf-8'))         
+        print(msg['extended_tweet']['full_text'])
+      else:
+        # add at the end of each tweet "t_end" 
+        self.client_socket\
+            .send(str(msg['text']+"t_end")\
+            .encode('utf-8'))
+        print(msg['text'])
+      return True
+    except BaseException as e:
+        print("Error on_data: %s" % str(e))
+    return True
+  def on_error(self, status):
+    print(status)
+    return True
+
+def sendData(c_socket, keyword):
+  print('start sending data from Twitter to socket')
+  # authentication based on the credentials
+  auth = OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_secret)
+  # start sending data from the Streaming API 
+  twitter_stream = Stream(auth, TweetsListener(c_socket))
+  twitter_stream.filter(track = keyword, languages=["en"])
+
+if __name__ == "__main__":
+    # server (local machine) creates listening socket
+    s = socket.socket()
+    host = "127.0.0.1"   
+    port = 5638
+    s.bind((host, port))
+    print('socket is ready')
+    # server (local machine) listens for connections
+    s.listen(4)
+    print('socket is listening')
+    # return the socket and the address on the other side of the connection (client side)
+    c_socket, addr = s.accept()
+    print("Received request from: " + str(addr))
+    # select here the keyword for the tweet data
+    sendData(c_socket, keyword = ['olympic'])
